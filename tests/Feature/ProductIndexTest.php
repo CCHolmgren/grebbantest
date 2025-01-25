6d66990a4ca81aae565b49078bf57c131bc995d3
Christoffer Magnedal <christoffer@humblebrag.se>
2025-01-25 21:17:22 +0100

    refactor: move fake http data to helper to clean up tests

--- tests/Feature/ProductIndexTest.php
+++ tests/Feature/ProductIndexTest.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Http;
 
-it('returns correctly formated data', function () {
+function fake_correct_data()
+{
     Http::fake([
         'https://draft.grebban.com/backend/products.json' => Http::response(
             '[{"id": 6267654,"name": "Auto Omega","attributes": {"cat": "cat_2","color": "black,white"}},{"id": 8094994,"name": "Bike Alef","attributes": {"color": "black,white","cat": "cat_1"}},{"id": 2846132,"name": "Bike Bet","attributes": {"color": "blue","cat": "cat_1_1,cat_1_2"}},{"id": 2169396,"name": "Auto Alpha","attributes": {"color": "green,blue","cat": "cat_2_2"}},{"id": 2749899,"name": "Auto Delta","attributes": {"color": "red","cat": "cat_2_2"}},{"id": 3311138,"name": "Auto Gamma","attributes": {"cat": "cat_2_3,cat_2_2","color": "black,white"}},{"id": 4364807,"name": "Bike Gimel","attributes": {"color": "red,white,green","cat": "cat_1_1,cat_1_2"}},{"id": 5385176,"name": "Bike Dalet","attributes": {"color": "black","cat": "cat_1_2"}},{"id": 12345,"name": "Random product","attributes": {}}]',
@@ -15,6 +16,26 @@ it('returns correctly formated data', function () {
             ['Content-Type' => 'application/json', 'X-Foo' => 'Bar']
         ),
     ]);
+}
+
+function fake_500_error()
+{
+    Http::fake([
+        'https://draft.grebban.com/backend/products.json' => Http::response(
+            '',
+            500,
+            ['Content-Type' => 'application/json']
+        ),
+        'https://draft.grebban.com/backend/attribute_meta.json' => Http::response(
+            '',
+            500,
+            ['Content-Type' => 'application/json', 'X-Foo' => 'Bar']
+        ),
+    ]);
+}
+
+it('returns correctly formated data', function () {
+    fake_correct_data();
 
     $response = $this->getJson('/product?page=1&page_size=2');
     $response->assertOk();
@@ -46,18 +67,7 @@ it('returns correctly formated data', function () {
 });
 
 it('defaults to sane values if page size is empty', function () {
-    Http::fake([
-        'https://draft.grebban.com/backend/products.json' => Http::response(
-            '[{"id": 6267654,"name": "Auto Omega","attributes": {"cat": "cat_2","color": "black,white"}},{"id": 8094994,"name": "Bike Alef","attributes": {"color": "black,white","cat": "cat_1"}},{"id": 2846132,"name": "Bike Bet","attributes": {"color": "blue","cat": "cat_1_1,cat_1_2"}},{"id": 2169396,"name": "Auto Alpha","attributes": {"color": "green,blue","cat": "cat_2_2"}},{"id": 2749899,"name": "Auto Delta","attributes": {"color": "red","cat": "cat_2_2"}},{"id": 3311138,"name": "Auto Gamma","attributes": {"cat": "cat_2_3,cat_2_2","color": "black,white"}},{"id": 4364807,"name": "Bike Gimel","attributes": {"color": "red,white,green","cat": "cat_1_1,cat_1_2"}},{"id": 5385176,"name": "Bike Dalet","attributes": {"color": "black","cat": "cat_1_2"}},{"id": 12345,"name": "Random product","attributes": {}}]',
-            200,
-            ['Content-Type' => 'application/json']
-        ),
-        'https://draft.grebban.com/backend/attribute_meta.json' => Http::response(
-            '[{"name": "Color","code": "color","values": [{"name": "Brown","code": "brown"},{"name": "Black","code": "black"},{"name": "White","code": "white"},{"name": "Blue","code": "blue"},{"name": "Green","code": "green"},{"name": "Red","code": "red"}]},{"name": "Category","code": "cat","values": [{"name": "Electric bikes","code": "cat_1_1"},{"name": "Bikes","code": "cat_1"},{"name": "BMX","code": "cat_1_2"},{"name": "Cars","code": "cat_2"},{"name": "Electric cars","code": "cat_2_1"},{"name": "Hybrids","code": "cat_2_2"},{"name": "Sportscars","code": "cat_2_3"}]}]',
-            200,
-            ['Content-Type' => 'application/json', 'X-Foo' => 'Bar']
-        ),
-    ]);
+    fake_correct_data();
 
     $response = $this->getJson('/product?page=1');
     $response->assertOk();
@@ -69,18 +79,7 @@ it('defaults to sane values if page size is empty', function () {
 });
 
 it('returns empty products if page outside range', function () {
-    Http::fake([
-        'https://draft.grebban.com/backend/products.json' => Http::response(
-            '[{"id": 6267654,"name": "Auto Omega","attributes": {"cat": "cat_2","color": "black,white"}},{"id": 8094994,"name": "Bike Alef","attributes": {"color": "black,white","cat": "cat_1"}},{"id": 2846132,"name": "Bike Bet","attributes": {"color": "blue","cat": "cat_1_1,cat_1_2"}},{"id": 2169396,"name": "Auto Alpha","attributes": {"color": "green,blue","cat": "cat_2_2"}},{"id": 2749899,"name": "Auto Delta","attributes": {"color": "red","cat": "cat_2_2"}},{"id": 3311138,"name": "Auto Gamma","attributes": {"cat": "cat_2_3,cat_2_2","color": "black,white"}},{"id": 4364807,"name": "Bike Gimel","attributes": {"color": "red,white,green","cat": "cat_1_1,cat_1_2"}},{"id": 5385176,"name": "Bike Dalet","attributes": {"color": "black","cat": "cat_1_2"}},{"id": 12345,"name": "Random product","attributes": {}}]',
-            200,
-            ['Content-Type' => 'application/json']
-        ),
-        'https://draft.grebban.com/backend/attribute_meta.json' => Http::response(
-            '[{"name": "Color","code": "color","values": [{"name": "Brown","code": "brown"},{"name": "Black","code": "black"},{"name": "White","code": "white"},{"name": "Blue","code": "blue"},{"name": "Green","code": "green"},{"name": "Red","code": "red"}]},{"name": "Category","code": "cat","values": [{"name": "Electric bikes","code": "cat_1_1"},{"name": "Bikes","code": "cat_1"},{"name": "BMX","code": "cat_1_2"},{"name": "Cars","code": "cat_2"},{"name": "Electric cars","code": "cat_2_1"},{"name": "Hybrids","code": "cat_2_2"},{"name": "Sportscars","code": "cat_2_3"}]}]',
-            200,
-            ['Content-Type' => 'application/json', 'X-Foo' => 'Bar']
-        ),
-    ]);
+    fake_correct_data();
 
     $response = $this->getJson('/product?page=100&page_size=10');
     $response->assertOk();
@@ -99,18 +98,7 @@ it('disallows non numeric values for input parameters', function () {
 });
 
 it('handles if server is missing', function () {
-    Http::fake([
-        'https://draft.grebban.com/backend/products.json' => Http::response(
-            '',
-            500,
-            ['Content-Type' => 'application/json']
-        ),
-        'https://draft.grebban.com/backend/attribute_meta.json' => Http::response(
-            '',
-            500,
-            ['Content-Type' => 'application/json', 'X-Foo' => 'Bar']
-        ),
-    ]);
+    fake_500_error();
 
     $response = $this->getJson('/product?page=100&page_size=10');
     $response->assertServiceUnavailable();
